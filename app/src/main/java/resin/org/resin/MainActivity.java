2f1ce653a8fc1fcda7428c1e760fdb48e732c69e
Ionica Madalin <ionicamadalin@gmail.com>
2018-02-17 18:31:15 +0200

    first commit

---
+++ app/src/main/java/resin/org/resin/MainActivity.java
@@ -0,0 +1,13 @@
+package resin.org.resin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
